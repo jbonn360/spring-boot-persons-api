33411a4ef7ea9d7a796cb725d84f2a127e4e8e66
Justin Bonnici <justinbonnici94@gmail.com>
2019-06-19 22:04:10 +0200

    Initial Commit

---
+++ src/main/java/com/example/demo/model/PersonWrapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class PersonWrapper {
+	private List<Person> persons;
+
+	public List<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+	
+	
+}
