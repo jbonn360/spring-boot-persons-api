58f3526ee339fc18e4c3b93f8faf1ef1eeb1e8e0
Justin Bonnici <justinbonnici94@gmail.com>
2019-06-21 22:31:05 +0200

    Add postgreSQL DB connectivity.
    Add api methods for loading all people and loading 1 person by id.

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -6,10 +6,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 import com.example.demo.model.Person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository("postgres")
-public class PersonDataAccessService implements PersonDao{
+public class PersonDataAccessService implements PersonDao {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public int insertPerson(UUID id, Person person) {
@@ -18,13 +28,24 @@ public class PersonDataAccessService implements PersonDao{
 
 	@Override
 	public List<Person> selectAllPeople() {
-		return Arrays.asList(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+		final String sql = "SELECT id, name FROM person";
+		List<Person> people = jdbcTemplate.query(sql, (resultSet, i) -> {
+			UUID id = UUID.fromString(resultSet.getString("id"));
+			String name = resultSet.getString("name");
+			return new Person(id, name);
+		});
+		return people;
 	}
 
 	@Override
 	public Optional<Person> selectPersonById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		final String sql = "SELECT id, name FROM person WHERE id = ?";
+		Person person = jdbcTemplate.queryForObject(sql, new Object[] { id }, (resultSet, i) -> {
+			UUID personId = UUID.fromString(resultSet.getString("id"));
+			String name = resultSet.getString("name");
+			return new Person(personId, name);
+		});
+		return Optional.ofNullable(person);
 	}
 
 	@Override
